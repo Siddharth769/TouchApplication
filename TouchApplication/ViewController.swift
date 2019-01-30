b5aee33c5bfc56e07402c001f6cc3b04692e0a86
Siddharth <sathesiddharth14@gmail.com>
2019-01-30 14:35:41 +0530

    Get touch count and method implemented.

--- TouchApplication/ViewController.swift
+++ TouchApplication/ViewController.swift
@@ -10,11 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var methodStatus: UILabel!
+    @IBOutlet weak var touchesStatus: UILabel!
+    @IBOutlet weak var tapsStatus: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touchCount = touches.count
+        let touch = touches.first
+        let tapCount = touch!.tapCount
+        
+        methodStatus.text = "touchesBegan"
+        touchesStatus.text = "\(touchCount) touches"
+        tapsStatus.text = "\(tapCount) taps"
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touchCount = touches.count
+        let touch = touches.first
+        let tapCount = touch!.tapCount
+        
+        methodStatus.text = "touchesMoved"
+        touchesStatus.text = "\(touchCount) touches"
+        tapsStatus.text = "\(tapCount) taps"
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touchCount = touches.count
+        let touch = touches.first
+        let tapCount = touch!.tapCount
+        
+        methodStatus.text = "touchesEnded";
+        touchesStatus.text = "\(touchCount) touches"
+        tapsStatus.text = "\(tapCount) taps"
+    }
+    
+    
 
 }
 
